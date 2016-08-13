7deefd2ffa32ec97c8fb70970524106de06a1b54
feldob <felix.dobslaw@miun.se>
2016-08-13 10:32:11 +0200

    simpified the bug to be recreated.

--- resource-bug/src/main/java/bug/InjectResource.java
+++ resource-bug/src/main/java/bug/InjectResource.java
@@ -1,13 +1,19 @@
 package bug;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 /**
- * Resource created with:
- * asadmin create-custom-resource --restype java.lang.String --factoryclass org.glassfish.resources.custom.factory.PrimitivesAndStringFactory --property value=wontshow customResource
+ * Resource created with: asadmin create-custom-resource --restype
+ * java.lang.String --factoryclass
+ * org.glassfish.resources.custom.factory.PrimitivesAndStringFactory --property
+ * value=wontshow customResource
+ * 
  * @author felix dobslaw
  *
  */
@@ -17,12 +23,14 @@ public class InjectResource {
 
 	public InjectResource() {
 	}
-	
+
 	@Resource(lookup = "customResource")
 	private String customResource;
-	
+//	private String customResource = "wontshow";
+
 	@PostConstruct
 	private void initValues() {
-		System.out.println(customResource);
+		Logger.getLogger(InjectResource.class.getName()).log(Level.WARNING, "The String will only show if not injected: " + customResource, customResource);
+		;
 	}
 }
